e49cc0d2acb5966e120db322ca2691f763f4315a
Aleksandr Kudriavtsev <aklightcode@gmail.com>
2021-03-03 13:56:25 +0300

    change const to let

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 // find the edit profile elements in the DOM
 
-const editProfileModal = document.querySelector('.modal_type_edit-profile');
-const editProfileForm = editProfileModal.querySelector('.modal__form');
-const editButton = document.querySelector('.profile__edit-button'); 
-const submitButton = document.querySelector('.modal__save'); 
-const inputName = document.querySelector('.modal__input_name'); 
-const inputDescription = document.querySelector('.modal__input_description'); 
-const profileName = document.querySelector('.profile__name'); 
-const profileDescription = document.querySelector('.profile__description'); 
+let editProfileModal = document.querySelector('.modal_type_edit-profile');
+let editProfileForm = editProfileModal.querySelector('.modal__form');
+let editButton = document.querySelector('.profile__edit-button'); 
+let submitButton = document.querySelector('.modal__save'); 
+let inputName = document.querySelector('.modal__input_name'); 
+let inputDescription = document.querySelector('.modal__input_description'); 
+let profileName = document.querySelector('.profile__name'); 
+let profileDescription = document.querySelector('.profile__description'); 
 
 function openPopup(modal) {
     modal.classList.add('modal_is-open');
@@ -42,7 +42,7 @@ function setEventListeners() {
 //edit info form handler
 setEventListeners();
 
-const userInfo = {title: profileName.textContent, description: profileDescription.textContent};
+let userInfo = {title: profileName.textContent, description: profileDescription.textContent};
 
 //open edit info form
 editButton.addEventListener('click', () => {

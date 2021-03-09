08f82839c133ee3997f3af848cbb1222bec9564c
Aleksandr Kudriavtsev <aklightcode@gmail.com>
2021-03-09 19:01:19 +0300

    fixed after the review

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,63 @@
 // find the edit profile elements in the DOM
 
-let editProfileModal = document.querySelector('.modal_type_edit-profile');
-let editProfileForm = editProfileModal.querySelector('.modal__form');
-let editButton = document.querySelector('.profile__edit-button'); 
-let submitButton = document.querySelector('.modal__save'); 
-let inputName = document.querySelector('.modal__input_name'); 
-let inputDescription = document.querySelector('.modal__input_description'); 
-let profileName = document.querySelector('.profile__name'); 
-let profileDescription = document.querySelector('.profile__description'); 
+let editProfileModal = document.querySelector(".modal");
+let editProfileForm = editProfileModal.querySelector(".modal__form");
+let editButton = document.querySelector(".profile__edit-button");
+let submitButton = document.querySelector(".modal__save");
+let inputName = document.querySelector(".modal__input_content_name");
+let inputDescription = document.querySelector(
+  ".modal__input_content_description"
+);
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
 
 function openPopup(modal) {
-    modal.classList.add('modal_is-open');
-    document.addEventListener('keyup', handleEscClose);
-};
+  modal.classList.add("modal_is-open");
+  document.addEventListener("keyup", handleEscClose);
+}
 
 function closePopup(modal) {
-    modal.classList.remove('modal_is-open');
-    document.removeEventListener('keyup', handleEscClose);
+  modal.classList.remove("modal_is-open");
+  document.removeEventListener("keyup", handleEscClose);
+}
+
+function openEditProfilePopup() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+  openPopup(editProfileModal);
+}
+
+let submitEditProfilePopup = (evt) => {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  closePopup(editProfileModal);
 };
 
 function handleEscClose(e) {
-    if (e.key === "Escape") {
-        closePopup(editProfileModal)
-    }
+  if (e.key === "Escape") {
+    closePopup(editProfileModal);
+  }
 }
 
-function handleEnterClose(e) {
-    if (e.key === "Enter") {
-        closePopup(editProfileModal)
-    }
+function closePopupByClick(e) {
+  if (
+    e.target.classList.contains("modal__close-btn") ||
+    e.target.classList.contains("modal_is-open")
+  ) {
+    closePopup(editProfileModal);
+  }
 }
 
 function setEventListeners() {
-    editProfileModal.addEventListener('click', (e) => {
-        if(e.target.classList.contains('modal__close-btn') || e.target.classList.contains('modal_is-open')) {
-            closePopup(editProfileModal);
-        }
-    })
-};
+  editProfileModal.addEventListener("click", closePopupByClick);
+}
 
 //edit info form handler
 setEventListeners();
 
-let userInfo = {title: profileName.textContent, description: profileDescription.textContent};
-
 //open edit info form
-editButton.addEventListener('click', () => {
-    inputName.value = userInfo.title;
-    inputDescription.value = userInfo.description;
-    openPopup(editProfileModal);
-    document.addEventListener('keyup', handleEnterClose);
-});
+editButton.addEventListener("click", openEditProfilePopup);
 
 //submit edit info form
-submitButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    userInfo.title = inputName.value;
-    userInfo.description = inputDescription.value;
-    profileName.textContent = userInfo.title;
-    profileDescription.textContent = userInfo.description;
-    closePopup(editProfileModal);
-});
+editProfileForm.addEventListener("submit", submitEditProfilePopup);
